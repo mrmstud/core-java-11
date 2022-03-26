f593d6e25fbb47cdf22981c855f448d1e3d7991c
mrmlabs <mrm.stud@gmail.com>
2022-03-26 14:56:31 -0500

    Formatted src file

--- src/main/java/com/java8learn/functions/PredicateDemo.java
+++ src/main/java/com/java8learn/functions/PredicateDemo.java
@@ -6,18 +6,15 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class PredicateDemo {
-	
-	
 
 	public static void main(String[] args) {
 		Predicate<String> metrocityPredicate = (cityName) -> {
-			List<String> metroCityList = Stream.of(METROCITIES.values())
-			.map(METROCITIES::name)
-			.collect(Collectors.toList());
-			
+			List<String> metroCityList = Stream.of(METROCITIES.values()).map(METROCITIES::name)
+					.collect(Collectors.toList());
+
 			return metroCityList.contains(cityName.toUpperCase());
 		};
-		
+
 		System.out.println("metrocityPredicate.test('Pune') :" + metrocityPredicate.test("Pune"));
 		System.out.println("metrocityPredicate.test('Akola') :" + metrocityPredicate.test("Akola"));
 	}
