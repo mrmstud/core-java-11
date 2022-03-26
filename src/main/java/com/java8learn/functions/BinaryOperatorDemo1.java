5ff46e6d38ac6d6fac78a27df5814a87943299ed
mrmlabs <mrm.stud@gmail.com>
2022-03-26 18:03:34 -0500

    BinaryOperatorDemo example

--- src/main/java/com/java8learn/functions/BinaryOperatorDemo1.java
+++ src/main/java/com/java8learn/functions/BinaryOperatorDemo1.java
@@ -4,11 +4,19 @@ import java.util.function.BinaryOperator;
 
 public class BinaryOperatorDemo1 {
 	public static void main(String args[]) {
-		BinaryOperator<Integer> binaryOperator = BinaryOperator
-				.maxBy((Integer num1, Integer num2) -> num1.compareTo(num2));
-
+		
+		System.out.println("BinaryOperatorDemo1:");
+		
+		BinaryOperator<Integer> addTwoInt = (a,b) -> a+b;
+		BinaryOperator<Integer> multiplyTwoInt = (a,b) -> a*b;
+		System.out.println("addTwoInt.apply(10, 20): " + addTwoInt.apply(10, 20));
+		System.out.println("multiplyTwoInt.apply(10, 20):" + multiplyTwoInt.apply(10, 20));		
+		
+		System.out.println("Returns a BinaryOperator which returns the greater of two elementsaccording to the specified Comparator:");
+		BinaryOperator<Integer> binaryOperator = BinaryOperator.maxBy(Integer::compareTo);
 		System.out.println("binaryOperator.apply(98, 11) : " + binaryOperator.apply(98, 11));
 		System.out.println("binaryOperator.apply(11, 11) : " + binaryOperator.apply(11, 11));
 		System.out.println("binaryOperator.apply(11, 98) : " + binaryOperator.apply(11, 98));
 	}
+
 }
